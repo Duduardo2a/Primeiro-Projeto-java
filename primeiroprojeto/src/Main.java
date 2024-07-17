a705357ff2b2119f60d9e492dd3a51c54f869640
Eduardo Araujo <eduardoaraujof2@gmail.com>
2024-07-17 01:08:04 -0300

    Add files via upload

---
+++ primeiroprojeto/src/Main.java
@@ -0,0 +1,32 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+
+        //calculation média das notas dos filmes
+        double media = (9.8 + 6.3 + 8.0) / 3;
+        System.out.println(media);
+        String sinopse;
+        sinopse = """
+        Filme Top Gun
+        Filme de aventura com galã dos anos 80
+        Muito bom
+        Ano de lançamento
+        2022
+        """;
+        System.out.println(sinopse);
+
+        int classification = (int) (media / 2);
+        System.out.println(classification);
+
+    }
+}
